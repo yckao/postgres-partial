b284f0f2e5c80581ecb587ed4ecb4b26faa5b224
yckao <yckaokarl@gmail.com>
2020-07-20 08:23:09 +0900

    test: still trying to get ts-jest work with github action

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  modulePathIgnorePatterns: ['.d.ts'],
-  collectCoverage: true,
-  globals: {
-    'ts-jest': {
-      packageJson: 'package.json'
-    }
-  }
+  collectCoverage: true
 }
